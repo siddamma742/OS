2a3952ceeca167752f5b57cece1bfb16f9c540d3
siddamma742 <siddamma742@gmail.com>
2026-03-09 13:04:46 +0530

    Add files via upload

---
+++ pri_preemtive.c
@@ -0,0 +1,73 @@
+#include <stdio.h>
+
+int main(){
+
+    int n,i,time=0,highest;
+    int at[20],bt[20],bt_copy[20],priority[20];
+    int ct[20],tat[20],wt[20];
+    int completed=0;
+    float avg_tat=0,avg_wt=0;
+
+    printf("Enter number of processes: ");
+    scanf("%d",&n);
+
+    for(i=0;i<n;i++){
+        printf("\nProcess P%d\n",i+1);
+
+        printf("Arrival Time: ");
+        scanf("%d",&at[i]);
+
+        printf("Burst Time: ");
+        scanf("%d",&bt[i]);
+
+        printf("Priority (Lower number = Higher priority): ");
+        scanf("%d",&priority[i]);
+
+        bt_copy[i] = bt[i];
+    }
+
+    while(completed != n){
+
+        highest=-1;
+
+        for(i=0;i<n;i++){
+            if(at[i] <= time && bt[i] > 0){
+                if(highest==-1 || priority[i] < priority[highest])
+                    highest=i;
+            }
+        }
+
+        if(highest==-1){
+            time++;
+            continue;
+        }
+
+        bt[highest]--;
+        time++;
+
+        if(bt[highest]==0){
+            completed++;
+            ct[highest]=time;
+        }
+    }
+
+    for(i=0;i<n;i++){
+        tat[i]=ct[i]-at[i];
+        wt[i]=tat[i]-bt_copy[i];
+
+        avg_tat+=tat[i];
+        avg_wt+=wt[i];
+    }
+
+    printf("\nPID\tAT\tBT\tPR\tCT\tTAT\tWT\n");
+
+    for(i=0;i<n;i++){
+        printf("P%d\t%d\t%d\t%d\t%d\t%d\t%d\n",
+        i+1,at[i],bt_copy[i],priority[i],ct[i],tat[i],wt[i]);
+    }
+
+    printf("\nAverage Turnaround Time = %.2f",avg_tat/n);
+    printf("\nAverage Waiting Time = %.2f\n",avg_wt/n);
+
+    return 0;
+}
